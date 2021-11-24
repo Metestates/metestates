f5f5a6ef2c6d8698f09aa96be9f00fd1a6c28d90
michael@zalla.io <michael@zalla.io>
2021-11-24 15:10:38 -0800

    Configures `apollo-client`;

---
+++ apollo.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	client: {
+		tagName: 'gql',
+		service: {
+			name: 'decentraland/marketplace',
+			url: 'https://api.thegraph.com/subgraphs/name/decentraland/marketplace',
+			headers: {},
+			skipSSLValidation: true
+		}
+	},
+}
