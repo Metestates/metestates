237377bd5f73f028c256e6733c85f077140867ab
michael@zalla.io <michael@zalla.io>
2021-11-24 16:40:15 -0800

    Creates `ApolloClient` and passes it to provider;

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+	uri: `https://api.thegraph.com/subgraphs/name/decentraland/marketplace`,
+	cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
+
+	document.getElementById('root')
+
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
