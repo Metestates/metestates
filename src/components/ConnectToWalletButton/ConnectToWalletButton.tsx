26f0c052a3416d9c87e59f8c8cbf1a04b93ed908
Michael Zalla <michael@zalla.io>
2022-08-12 16:45:18 -0700

    Extracts component to file;

--- src/components/ConnectToWalletButton/ConnectToWalletButton.tsx
+++ src/components/ConnectToWalletButton/ConnectToWalletButton.tsx
@@ -5,69 +5,7 @@ import {
 	useNotifications,
 } from '@usedapp/core'
 
-import { formatUnits } from '@ethersproject/units'
-
-import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
-
-import useTokenBalance from '../../hooks/use-token-balance'
-
-// See: https://etherscan.io/token/0x0f5d2fb29fb7d3cfee444a200298f468908cc942
-const ManaErc20TokenAddress = `0x0f5d2fb29fb7d3cfee444a200298f468908cc942`
-const ManaErc20TokenDigits = 18
-
-type TokenBalanceProps = {
-	address: string,
-}
-
-const TokenBalance: React.FC<TokenBalanceProps> = ({ address }) => {
-
-	const [
-		tokenBalance,
-		tokenBalanceError,
-		tokenBalanceLoading,
-	] = useTokenBalance(ManaErc20TokenAddress, address)
-
-	if(
-		tokenBalanceLoading ||
-		(
-			!tokenBalance &&
-			!tokenBalanceError
-		)
-	) {
-		return (
-			<div>
-				<p>Loading…</p>
-			</div>
-		)
-	}
-
-	if(tokenBalanceError) {
-		return (
-			<div>
-				<p>Error retreiving token balance: {tokenBalanceError}</p>
-			</div>
-		)
-	}
-
-	let formattedTokenBalance = formatUnits(tokenBalance!, ManaErc20TokenDigits)
-
-	return (
-		<div>
-			{
-				address &&
-				<>
-					<p>Address: {address}</p>
-					<Jazzicon diameter={48} seed={jsNumberForAddress(address)} />
-				</>
-			}
-			{
-				tokenBalance &&
-				<p>Balance: {formattedTokenBalance} MANA</p>
-			}
-		</div>
-	)
-
-}
+import TokenBalance from '../TokenBalance/TokenBalance'
 
 const ConnectToWalletButton = () => {
 
